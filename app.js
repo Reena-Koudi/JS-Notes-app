f61c7a0a2d4de3fe9870797924c0ec7240258763
Reenakoudi <kreena92@gmail.com>
2018-06-20 14:05:32 +0100

    Added notes.js

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+const argv = yargs.argv;
 var command = process.argv[2];
 console.log('Command: ', command);
-console.log(process.argv);
+console.log('Process', process.argv);
+console.log('Yargs', argv);
 
 if(command === 'add') {
   console.log('Addind new note');
