0911865699206bbbc18e0ce7b71cc313b74f00cb
Reenakoudi <kreena92@gmail.com>
2018-06-20 12:20:33 +0100

    Getting input fromn user on command line

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
 
-var user = os.userInfo();
-console.log(user);
+const notes = require('./notes.js');
 
-//fs.appendFileSync('greetings.txt', 'Hello World!');
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log(process.argv);
+
+if(command === 'add') {
+  console.log('Addind new note');
+} else if (command === 'list'){
+  console.log('Listing all notes');
+} else if (command === 'read'){
+  console.log('Reading note')
+} else if (command === 'remove'){
+  console.log('Removing note')
+}
+else {
+  console.log('Command not recognised');
+}
