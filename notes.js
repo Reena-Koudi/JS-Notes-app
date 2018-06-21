411bb32e1f3c61de7e2c12184e832ec4e62a6647
Reenakoudi <kreena92@gmail.com>
2018-06-21 15:58:11 +0100

    Refactoring to make fetchNotes and saveNotes functions

--- notes.js
+++ notes.js
@@ -2,23 +2,34 @@ console.log('Starting notes.js');
 
 const fs = require('fs');
 
+const fetchNotes = () => {
+  try {
+    const notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
+};
+
+const saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+
 const addNote = (title, body) => {
-  let notes = [];
+  const notes = fetchNotes();
   const note = {
     title,
     body
   };
 
 
-  const notesString = fs.readFileSync('notes-data.json');
-  notes = JSON.parse(notesString);
-
-
   const duplicateNotes = notes.filter(note => note.title === title);
 
   if (duplicateNotes.length === 0) {
     notes.push(note);
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    saveNotes(notes);
+    return note;
   }
 };
 
